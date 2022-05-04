0f15f35929d9f8b153a0c7ecdfd282b3c40a217a
haiha210 <haiha210.gm@gmail.com>
2022-05-04 15:01:30 +0700

    demo action cable

---
+++ src/pages/user.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { ActionCableProvider } from 'react-actioncable-provider';
+
+import { createConsumer } from "@rails/actioncable"
+
+class User extends Component {
+  constructor() {
+    super()
+    this.cable = createConsumer('ws://localhost:28080/cable')
+  }
+
+  componentDidMount() {
+    this.createSubscription()
+  }
+
+  createSubscription = () => {
+    this.subscriptions = this.cable.subscriptions.create(
+      { channel: 'UserChannel' },
+      {
+        connected: () => {
+          console.log("user channel connected");
+        },
+        received: message => this.handleReceivedMessages(message),
+      }
+    )
+  }
+
+  mapMessages = () => {
+    return this.state.messages.map((message, i) =>
+      <li key={i}>{message.content}</li>)
+  }
+
+  handleReceivedMessages = message => {
+    console.log(message)
+  }
+
+  render() {
+    return (
+      <div>
+        <ActionCableProvider cable={this.cable}/>
+        <p>Player</p>
+      </div>
+    );
+  }
+}
+
+export default User;
